f394b343d6337cc3d029132b9b89a384ee48e9c0
matheusAFreitas <matheus.freitas@fcamara.com.br>
2023-10-15 15:22:23 -0300

    fix: an import that I forgot lol

--- src/modules/appointments/routes/appointment.routes.ts
+++ src/modules/appointments/routes/appointment.routes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 
-import CreateAppointmentService from '../services/CreateAppointmentService';
-import { GetAppointmentByUser } from '../services';
+import { CreateAppointmentService, GetAppointmentByUser } from '../services';
 
 const appointmentRoutes = Router();
 
